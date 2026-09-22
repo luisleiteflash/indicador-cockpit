d938f6cfe5b9a3d1114a03f57b2390b427b4c2b0
luisleiteflash <luis.leite@move3.com.br>
2026-09-22 10:00:10 -0300

    Atualiza dados.js - 22/09/2026 10:00

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 /* gerado automaticamente a partir do PowerPoint - nao editar a mao */
 window.COCKPIT = {
-  atualizado: "22/09/2026 09:55",
+  atualizado: "22/09/2026 10:00",
   arquivo: "B2B_RESA_Indicadores_Pipeline_21.09.26_v3.pptx",
   semana: "21/09/2026",
   titulo: "Cockpit Comercial",
